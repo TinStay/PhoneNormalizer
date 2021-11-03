37867d9730cc08c396da5033c107d66ee34fc268
Martin <marti.staykov@gmail.com>
2021-11-03 09:42:33 +0100

    Add phone formatting

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bytes"
+	"regexp"
+)
+
+func normalize(phone string) string {
+	var buf bytes.Buffer
+
+	for _, ch := range phone{
+		if ch >= '0' && ch <= '9'{
+			buf.WriteRune(ch)
+		}
+	}
+
+	return buf.String()
+}
+
+func normalizeRegex(phone string) string {
+	regex := regexp.MustCompile("[\\D]")
+
+	
+	return regex.ReplaceAllString(phone, "")
+}
