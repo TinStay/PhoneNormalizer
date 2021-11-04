1ed06e9b9e028fac13c7ff56bfb286f58826655c
Martin <marti.staykov@gmail.com>
2021-11-04 19:05:18 +0100

    Create Postgresql table for phone numbers

--- main.go
+++ main.go
@@ -12,38 +12,52 @@ import (
 const (
 	host     = "localhost"
 	port     = 5432
-	user     = "tinstay"
-	password = ""
+	user     = "postgres"
+	password = "1234"
 	dbname   = "phoneDB"
 )
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
-	fmt.Println(psqlInfo)
 
 	// Open database
 	db, err := sql.Open("postgres", psqlInfo)
-
-	if err != nil {
-		panic(err)
-	}
+	must(err)
 
 	err = createDB(db, dbname)
+	must(err)
 
-	if err != nil {
-		panic(err)
-	}
+	must(createPhoneNumbersTable(db))
+
 	db.Close()
 }
 
+func createPhoneNumbersTable(db *sql.DB) error{
+	statement := `
+	CREATE TABLE IF NOT EXISTS phone_numbers (
+		ID SERIAL,
+		value VARCHAR(255)
+	)`
+
+	_, err := db.Exec(statement)
+
+	return err
+}
+
 func createDB(db *sql.DB, name string) error {
-	_, err := db.Exec("CREATE DATABASE" + name)
+	_, err := db.Exec("CREATE DATABASE " + name)
 
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func must(err error){
+	if err != nil{
+		panic(err)
+	}
 }
 
 func normalize(phone string) string {
