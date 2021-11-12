4fe524fb602b2b247c4de07075d0d15400fe55f4
Martin <marti.staykov@gmail.com>
2021-11-12 14:49:46 +0100

    Add update and delete func

--- main.go
+++ main.go
@@ -29,16 +29,133 @@ func main() {
 	// must(err)
 
 	// Create database table
-	// must(createPhoneNumbersTable(db))
+	must(createPhoneNumbersTable(db))
 
 	// Add phone to table
-	id, err := insertPhone(db, "1234567890")
+	// _, err = insertPhone(db, "1234567890")
+	// must(err)
+	// _, err = insertPhone(db, "123 456 7891")
+	// must(err)
+	// _, err = insertPhone(db, "(123) 456 7892")
+	// must(err)
+	// _, err = insertPhone(db, "(123) 456-7893")
+	// must(err)
+	// _, err = insertPhone(db, "123-456-7890")
+	// must(err)
+	// _, err = insertPhone(db, "1234567892")
+	// must(err)
+	// _, err = insertPhone(db, "(123)456-7892")
+	// must(err)
+
+	// number, err := getPhone(db, id)
+	// must(err)
+	// fmt.Println("Number is: ", number)
+
+	phones, err := getAllNumbers(db)
 	must(err)
-	fmt.Printf("New id:%d\n",id)
+
+	for _, p := range phones{
+		fmt.Printf("Working on... %v\n", p.number)
+		// Format number
+		number := normalize(p.number)
+
+		if number != p.number {
+			fmt.Println("Updating or removing...", number)
+			// Find current number
+			existing, err := findPhone(db, number)
+			must(err)
+
+			if existing != nil {
+				// delete number
+				must(deletePhone(db, p.id))
+			}else {
+				// update number
+				p.number = number
+				must(updatePhone(db, p))
+			}
+		} else {
+			fmt.Println("No changes required")
+		}
+	}
+
+	// Close db
 	db.Close()
 }
 
-func createPhoneNumbersTable(db *sql.DB) error{
+type phone struct{
+	id int
+	number string
+}
+
+func updatePhone(db *sql.DB, p phone) error {
+	statement := `UPDATE phone_numbers SET value=$2 WHERE id=$1`
+	_, err := db.Exec(statement, p.id, p.number)
+	return err
+}
+
+func deletePhone(db *sql.DB, id int) error {
+	statement := `DELETE FROM phone_numbers WHERE id=$1`
+	_, err := db.Exec(statement, id)
+	return err
+}
+
+func findPhone(db *sql.DB, number string)(*phone, error){
+	var p phone
+
+	row := db.QueryRow("Select value FROM phone_numbers WHERE id=$1", number)
+	err := row.Scan(&p.id, &p.number) 
+
+	if err != nil {
+		if err == sql.ErrNoRows{
+			return nil, nil
+		}else {
+			return nil, err
+		}
+	}
+
+	return &p, nil
+}
+
+func getAllNumbers(db *sql.DB)([]phone, error){
+	rows, err := db.Query("SELECT id, value FROM phone_numbers")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var ret []phone
+
+	// Loop through all rows
+	for rows.Next(){
+		var p phone
+		if err := rows.Scan(&p.id,&p.number,); err != nil {
+			return nil, err
+		}
+		ret = append(ret, p)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return ret, nil
+}
+
+func getPhone(db *sql.DB, id int)(string, error){
+	var number string
+
+	row := db.QueryRow("Select value FROM phone_numbers WHERE id=$1", id)
+	err := row.Scan(&number) 
+
+	if err != nil {
+		return "", err
+	}
+
+	return number, nil
+}
+
+func createPhoneNumbersTable(db *sql.DB) error {
 	statement := `
 	CREATE TABLE IF NOT EXISTS phone_numbers (
 		ID SERIAL,
